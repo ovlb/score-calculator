0f2e9457683b39f57f329f59fe1e1fa0468578f8
Oscar <o@ovl.design>
2020-09-17 02:31:32 +0200

    🧰 add rollup config

---
+++ src/entry.js
@@ -0,0 +1,39 @@
+// Import vue component
+import component from '@/components/ScoreCalculator.vue'
+
+// install function executed by Vue.use()
+const install = function installScoreCalculator(Vue) {
+  if (install.installed) return
+
+  install.installed = true
+  Vue.component('ScoreCalculator', component)
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+  install
+}
+
+// To auto-install on non-es builds, when vue is found
+// eslint-disable-next-line no-redeclare
+/* global window, global */
+if ('false' === process.env.ES_BUILD) {
+  let GlobalVue = null
+
+  if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue
+  } else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue
+  }
+
+  if (GlobalVue) {
+    GlobalVue.use(plugin)
+  }
+}
+
+// Inject install function into component - allows component
+// to be registered via Vue.use() as well as Vue.component()
+component.install = install
+
+// Export component by default
+export default component
